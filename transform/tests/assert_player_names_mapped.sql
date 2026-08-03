7177e4f320b7c029264b20139d82c5733ec7e21b
minhld06 <duyminhle153@gmail.com>
2026-08-03 11:34:36 +0700

    fix: rewrite assert_player_names_mapped for the new identity architecture

--- transform/tests/assert_player_names_mapped.sql
+++ transform/tests/assert_player_names_mapped.sql
@@ -1,13 +1,37 @@
 {{ config(severity='warn') }}
 
-select source, raw_player_name
+with understat_check as (
+    select
+        'understat' as source,
+        u.raw_player_name,
+        coalesce(pm.player_id, p.player_id) as player_id
+    from {{ ref('stg_understat__player_stats') }} u
+    left join {{ ref('player_name_map') }} pm
+        on pm.source = 'understat'
+       and pm.raw_player_name = u.raw_player_name
+       and pm.team_id = u.team_id
+    left join {{ ref('players') }} p
+        on {{ normalize_player_name('p.player_name') }} = {{ normalize_player_name('u.raw_player_name') }}
+),
+
+statbunker_check as (
+    select
+        'statbunker' as source,
+        s.raw_player_name,
+        coalesce(pm.player_id, p.player_id) as player_id
+    from {{ ref('stg_statbunker__player_stats') }} s
+    left join {{ ref('player_name_map') }} pm
+        on pm.source = 'statbunker'
+       and pm.raw_player_name = s.raw_player_name
+       and pm.team_id = s.team_id
+    left join {{ ref('players') }} p
+        on {{ normalize_player_name('p.player_name') }} = {{ normalize_player_name('s.raw_player_name') }}
+)
+
+select distinct source, raw_player_name
 from (
-    select 'statbunker' as source, raw_player_name, player_id
-    from {{ ref('stg_statbunker__player_stats') }}
-
+    select * from understat_check
     union all
-
-    select 'understat' as source, raw_player_name, player_id
-    from {{ ref('stg_understat__player_stats') }}
+    select * from statbunker_check
 ) unmapped_check
 where player_id is null
